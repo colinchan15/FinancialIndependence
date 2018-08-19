89c3c1afc48d28efeb4b53c328e252aea7fc369d
Colin MBP <cchan827@uwo.ca>
2018-08-19 03:03:04 -0400

    more polish on chequing and savings, implemented HIS case

--- src/main/java/Assets.java
+++ src/main/java/Assets.java
@@ -1,26 +1,25 @@
 package main.java;
 
-import java.sql.SQLOutput;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
-
-import static java.lang.Double.*;
 
 //@SuppressWarnings("Duplicates")
 public class Assets {
 
     private static Scanner scanner = new Scanner(System.in);
     private static HashMap <String, Double> hmAssets = new HashMap();
+    private static HashMap <String, Double> hmInterest = new HashMap();
 
     public static void main(String[] args) {
         Assets assets = new Assets();
         LinkedList linkedList = new LinkedList();
         linkedList.add(1);
         linkedList.add(2);
+        linkedList.add(3);
         System.out.println(assets.assets(linkedList));
         assets.displayAssets();
+        assets.displayInterests();
 
     }
 
@@ -36,7 +35,7 @@ public class Assets {
                     scanner.nextLine();
                     System.out.println("Please enter the name of the account and its current balance separated by a comma");
                     Double chequingTotal = 0.0;
-                    Double chequingBalance = assets.setHmAssets(chequingAccNum, hmAssets, chequingTotal);
+                    Double chequingBalance = assets.setHmAssets(false, chequingAccNum, hmAssets, hmInterest, chequingTotal);
                     assetsTotal += chequingBalance;
                     break;
                 case 2:
@@ -45,11 +44,18 @@ public class Assets {
                     scanner.nextLine();
                     System.out.println("Please enter the name of the account and its current balance separated by a comma");
                     double savingsTotal = 0.0;
-                    Double savingsBalance = assets.setHmAssets(savingsAccNum, hmAssets, savingsTotal);
+                    Double savingsBalance = assets.setHmAssets(true, savingsAccNum, hmAssets, hmInterest, savingsTotal);
                     assetsTotal += savingsBalance;
                     break;
                 case 3:
-                    System.out.println("");
+                    System.out.println("How many high-interest savings accounts do you have?");
+                    int HISavingsAccNum = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Please enter the name of the account, its current balance, and its interest rate separated by a comma");
+                    double HISavingsTotal = 0.0;
+                    Double HISavingsBalance = assets.setHmAssets(true, HISavingsAccNum, hmAssets, hmInterest, HISavingsTotal);
+                    assetsTotal += HISavingsBalance;
+                    break;
             }
         }
         return assetsTotal;
@@ -62,17 +68,32 @@ public class Assets {
         }
     }
 
+    public static void displayInterests(){
+        System.out.println();
+        for(HashMap.Entry <String, Double> entry : hmInterest.entrySet()){
+            System.out.println(entry.getKey() + ": %" + entry.getValue());
+        }
+    }
+
     public HashMap<String, Double> getHmAssets (){
         return hmAssets;
     }
 
-    public static double setHmAssets(int numOfAccounts, HashMap<String, Double> hmAssets, Double assetsTotal) {
-        for(int i = 0; i < numOfAccounts; i++){
+    public static double setHmAssets(boolean notChequing, int numOfAccounts, HashMap<String, Double> hmAssets, HashMap <String, Double> hmInterests, Double assetsTotal) {
+        for (int i = 0; i < numOfAccounts; i++) {
             String assetsInput = scanner.nextLine();
-            String [] assetsSplit = assetsInput.replaceAll("\\s","").split(",");
+            String[] assetsSplit = assetsInput.replaceAll("\\s", "").split(",");
             String assetsAccName = assetsSplit[0];
             Double assetsAccBalance = Double.parseDouble(assetsSplit[1]);
-            hmAssets.put(assetsAccName,assetsAccBalance);
+            Double assetsAccInterest = 0.0;
+            if (notChequing == true) {
+                assetsAccInterest = Double.parseDouble(assetsSplit[2]);
+            }
+//            } else if (!notChequing) {
+//                assetsAccInterest = 0.0;
+//            }
+            hmAssets.put(assetsAccName, assetsAccBalance);
+            hmInterests.put(assetsAccName, assetsAccInterest);
             assetsTotal += assetsAccBalance;
         }
         return assetsTotal;
