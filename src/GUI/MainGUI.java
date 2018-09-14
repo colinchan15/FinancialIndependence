e50aee623afff8d3b0f2297ab5e9a98e3c8ccd49
Colin MBP <cchan827@uwo.ca>
2018-09-14 11:20:31 -0400

    messing with scenebuilder javafx

---
+++ src/GUI/MainGUI.java
@@ -0,0 +1,20 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class MainGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+
+    }
+}
