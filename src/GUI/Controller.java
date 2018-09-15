b5a3d34348d729245c379254de4ec2858bf61221
Colin MBP <cchan827@uwo.ca>
2018-09-14 21:55:47 -0400

    minor changes to Controller.java

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -3,7 +3,6 @@ package GUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,8 +13,6 @@ import javafx.stage.Stage;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class Controller {
 
